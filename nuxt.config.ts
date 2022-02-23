9e3707235f9098b3717cd7398e01365b15dc2265
Caitlin Hawley <caitlin.hawley@quore.com>
2022-02-23 10:16:35 -0600

    Added sections in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 
+    // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#modules
+    modules: [
+    ],
+
+    // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#buildmodules
+    buildModules: [
+        // '@nuxtjs/tailwindcss'
+    ]
+
 })
